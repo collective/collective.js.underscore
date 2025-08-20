f64a59a03b181e114987db79d31c39570e98a10b
Laurent Lasudry <lasudry@gmail.com>
2025-08-20 14:31:17 +0200

    Add Plone 6.1 compatibility, drop Plone 4 compatibility (#7)
    Also update underscore.js to 1.13.7
    Also migrate package from flat-layout to src-layout
    Also add tests
    Also restore full history

--- setup.py
+++ setup.py
@@ -1,34 +1,52 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.5.2.2.dev0'
+version = '1.13.7.dev0'
 
 setup(name='collective.js.underscore',
       version=version,
       description="underscore.js for plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
         "Framework :: Plone",
-        "Programming Language :: Python",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.1",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='javascript plone underscore',
       author='Denis Krienbuehl',
       author_email='denis.krienbuehl@gmail.com',
       url='https://github.com/collective/collective.js.underscore',
       license='MIT',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective', 'collective.js'],
+      packages=find_packages('src', exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      package_dir={'': 'src'},
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'setuptools',
       ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ],
+      },
       entry_points="""
       # -*- Entry points: -*-
 
